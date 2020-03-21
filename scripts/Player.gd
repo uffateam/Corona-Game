b18ca91b65567a660d0ad22323d86abb628d21e5
Antônio Vinicius <antoniovinicius1801@gmail.com>
2020-03-21 16:41:43 -0300

    Primeira versão

---
+++ scripts/Player.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+export (int) var run_speed = 200
+export (int) var jump_speed = -400
+export (int) var gravity = 800
+
+var velocity = Vector2()
+var jumping = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	velocity.x = 0
+	
+	if Input.is_action_just_pressed("ui_select"):
+		jumping = true
+		velocity.y = jump_speed
+	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= run_speed
+		$Sprite.flip_h = true
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x += run_speed
+		$Sprite.flip_h = false
+	
+	velocity.y += delta * gravity
+	
+	if jumping and is_on_floor():
+		jumping = false
+	
+	velocity = move_and_slide(velocity, Vector2(0, -1))
