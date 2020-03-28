7baa3757d1a861f5ae6b52c21f4e62cc6bd64d7e
m-a-t-h-e-u-s <62441093+m-a-t-h-e-u-s@users.noreply.github.com>
2020-03-28 00:42:57 -0300

    Add files via upload

--- scripts/runner/Player.gd
+++ scripts/runner/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 export (int) var run_speed = 400
 export (int) var jump_speed = -400
 export (int) var gravity = 800
-export (int) var friction = 90
+export (int) var friction =120
 
 var jumping = false
 var hooking = false
@@ -26,29 +26,28 @@ func _input(event):
 	var norma
 	if (event is InputEventMouseButton):
 		var Posi_player = get("position")
-		if (event.button_index == BUTTON_LEFT and event.pressed):
-			Posi_Esquerdo = event.position
-			Posi_rel = Posi_Esquerdo - Posi_player
-			norma = sqrt((Posi_rel[0] * Posi_rel[0]) + (Posi_rel[1] * Posi_rel[1]))
-			Posi_rel = Posi_rel / norma
-			shot = true
-
-			$Gancho/CollisionShape2D/Sprite.show()
-			if Posi_rel[0] > 0:
-				$Gancho.rotation_degrees = -90 + atan((Posi_rel[1] / Posi_rel[0])) * 180 / PI
+		if (event.button_index == BUTTON_LEFT):
+			if event.pressed:
+				Posi_Esquerdo = event.position
+				Posi_rel = Posi_Esquerdo - Posi_player
+				norma = sqrt((Posi_rel[0] * Posi_rel[0]) + (Posi_rel[1] * Posi_rel[1]))
+				Posi_rel = Posi_rel / norma
+				shot = true
+				$Gancho/CollisionShape2D/Sprite.show()
+				if Posi_rel[0] > 0:
+					$Gancho.rotation_degrees = -90 + atan((Posi_rel[1] / Posi_rel[0])) * 180 / PI
+				else:
+					$Gancho.rotation_degrees = 90 + atan((Posi_rel[1] / Posi_rel[0])) * 180 / PI
 			else:
-				$Gancho.rotation_degrees = 90 + atan((Posi_rel[1] / Posi_rel[0])) * 180 / PI
-	else:
-		shot = false
-		hooking = false
-		Posi_Direito = Vector2()
-		Posi_Esquerdo = Vector2()
-		
+				hooking = false
+				shot = false
+				Posi_Direito = Vector2()
+				Posi_Esquerdo = Vector2()
+
 func _physics_process(delta):
 	if shot == true:
 		if len($Gancho.get_overlapping_bodies()) == 1:
-			$Gancho/CollisionShape2D.scale.y += 1
-
+			$Gancho/CollisionShape2D.scale.y += 1 #* delta
 		else:
 			shot = false
 			hooking = true
@@ -60,43 +59,36 @@ func _physics_process(delta):
 			Posi_Esquerdo = Vector2()
 			if $Gancho/CollisionShape2D.scale.y > 1:
 				$Gancho/CollisionShape2D.scale.y = $Gancho/CollisionShape2D.scale.y * 0.5
-				
 			else:
-
 				$Gancho/CollisionShape2D/Sprite.hide()
 		if (rasteira == false):
 			velocity.x = 0
 		else:
-			if (velocity.x == 0):
-				rasteira = false
 			if (velocity.x > 0):
 				velocity.x -= friction * delta
 			else:
 				velocity.x += friction * delta
 	else:
 		gravity = 0
-		velocity.x = 400 * Posi_rel[0]
-		velocity.y = 400 * Posi_rel[1]
+		velocity.x = 800 * Posi_rel[0]
+		velocity.y = 800 * Posi_rel[1]
 		if $Gancho/CollisionShape2D.scale.y > 1:
-			$Gancho/CollisionShape2D.scale.y -= 0.4 #0 * atan((Posi_rel[1] / Posi_rel[0]))
-
+			$Gancho/CollisionShape2D.scale.y -= 23.5 * delta 
 		else:
 			velocity.y = 0
 			velocity.x = 0
 			$Gancho/CollisionShape2D.scale.y = 1
-	if (Input.is_action_pressed("ui_up")) and (jumping == false):
-		shot = false
-		hooking = false
-		jumping = true
-		velocity.y = jump_speed
+			shot = false
+	
 	if (is_on_floor()):
 		jumping = false
-		friction = 90
-	else:
-		friction = 0
+		if (Input.is_action_pressed("ui_up")) and (jumping == false):
+			jumping = true		
+			velocity.y = jump_speed
+		
 	if (Input.is_action_pressed("ui_left")):
 		hooking = false
-		shot = false
+	
 		if (rasteira == false) and (lowered == false):
 			velocity.x -= run_speed
 			$AnimatedSprite2.flip_h = true
@@ -121,7 +113,7 @@ func _physics_process(delta):
 				$Collision.scale.y = 0.7
 				$AnimatedSprite2.play("Abaixado")
 	elif (Input.is_action_pressed("ui_right")):
-		shot = false
+		
 		hooking = false
 		if (rasteira == false) and (lowered == false):
 			velocity.x += run_speed
@@ -149,7 +141,6 @@ func _physics_process(delta):
 	elif (Input.is_action_pressed("ui_down")):
 		lowered = true
 		rasteira = false
-		shot = false
 		$Collision.position.x = 1
 		$Collision.position.y = 5
 		$Collision.scale.x = 0.8
